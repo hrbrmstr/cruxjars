85f51ca424543c9fb57ea6805fc2d893c7ba7406
boB Rudis <bob@rud.is>
2019-03-01 04:04:58 -0500

    R package repo initialization complete

---
+++ R/cruxjars-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/cruxjars>
+#' - BugReports: <https://gitlab.com/hrbrmstr/cruxjars/issues>
+#' 
+#' @md
+#' @name cruxjars
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
